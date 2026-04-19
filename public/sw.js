f6fdfb1e1c82e74b10b0ed526c9e8f0a150016d9
[dyad] <etienne.gauthiermanuel@gmail.com>
2026-04-19 12:46:58 +0200

    [dyad] Adding offline support and PWA capabilities - wrote 3 file(s)

---
+++ public/sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'ae86-chime-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/ae86_chime.mp3',
+  '/manifest.json',
+  '/placeholder.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
